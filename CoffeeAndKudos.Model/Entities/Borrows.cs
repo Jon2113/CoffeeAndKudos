e318906f7588f3be6cab4e06c1a3b5a365d4f80d
Jonas Peifer <you@example.com>
2026-04-28 10:23:46 +0200

    comments for all entities

--- CoffeeAndKudos.Model/Entities/Borrows.cs
+++ CoffeeAndKudos.Model/Entities/Borrows.cs
@@ -1,21 +1,37 @@
 namespace CoffeeAndKudos.Model.Entities;
 
+// Represents a single borrow record, i.e. one person lending something to another
 public class Borrow
 {
+    // Default constructor 
     public Borrow()
     {
     }
 
+    // Constructor used when we already have an ID 
     public Borrow(Guid borrowId)
     {
         BorrowId = borrowId;
     }
 
+    // Unique ID for this borrow record, automatically generated if not provided
     public Guid BorrowId { get; set; } = Guid.NewGuid();
+
+    // The person who is lending the item
     public Guid LenderId { get; set; }
+
+    // The person who is borrowing the item
     public Guid BorrowerId { get; set; }
+
+    // The name of the item being borrowed (e.g. "book", "charger")
     public string ItemName { get; set; } = string.Empty;
+
+    // Optional deadline for when the item should be returned; null means no due date set
     public DateOnly? DueDate { get; set; }
+
+    // The date and time the item was actually returned; null means it hasn't been returned yet
     public DateTime? ReturnedAt { get; set; }
+
+    // The date and time this borrow record was created; automatically set to now (UTC) when created
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
--- CoffeeAndKudos.Model/Entities/Favors.cs
+++ CoffeeAndKudos.Model/Entities/Favors.cs
@@ -1,20 +1,34 @@
 namespace CoffeeAndKudos.Model.Entities;
 
+// Represents a single favor record
 public class Favor
 {
+    // Default constructor 
     public Favor()
     {
     }
 
+    // Constructor used when we already have an ID 
     public Favor(Guid favorId)
     {
         FavorId = favorId;
     }
 
+    // Unique ID for this favor record 
     public Guid FavorId { get; set; } = Guid.NewGuid();
+
+    // The person who owes the favor
     public Guid DebtorId { get; set; }
+
+    // The person who is owed the favor
     public Guid CreditorId { get; set; }
+
+    // A description of what the favor is (e.g. "helped me move", "covered my lunch")
     public string Description { get; set; } = string.Empty;
+
+    // Whether the favor has been settled, false means it's still outstanding, true means it's been repaid
     public bool IsSettled { get; set; }
+
+    // The date and time this favor record was created, automatically set to now (UTC) when created
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
